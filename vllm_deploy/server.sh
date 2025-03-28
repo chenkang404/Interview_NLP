8224306ea093857ee3d19f5bdd38e3d637de76bf
wangchenkang <763711755@qq.com>
2025-03-28 11:00:38 +0800

    update code vllm deploy

--- vllm_deploy/server.sh
+++ vllm_deploy/server.sh
@@ -1,6 +1,6 @@
 # ps -ef |grep /root/miniconda3/envs/MiniCPMV2/bin/vllm |grep -v grep |awk -F" "  {'print $2'}|xargs -i kill -9 {}
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=2,3
 
 nohup python -m vllm.entrypoints.openai.api_server \
     --model /workspace/HOSTDIR/model_path/Qwen/Qwen2.5-32B-Instruct \
@@ -10,7 +10,7 @@ nohup python -m vllm.entrypoints.openai.api_server \
     --gpu-memory-utilization 0.90 \
     --max-num-seqs 8 \
     --trust-remote-code \
-    --tensor-parallel-size 1 \
+    --tensor-parallel-size 2 \
     --port 5115 \
     --host 0.0.0.0 \
     > ./vllm_qwen2-5-32B.log 2>&1 &
